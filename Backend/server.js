82fb42c6cf4109cde574fb7e39fd4f7a9c805d1e
Saikat Bahadur <saikatbahadur1234@gmail.com>
2025-05-18 20:03:20 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -13,13 +13,9 @@ const startServer = async () => {
     await connectCloudinary();
     console.log("✅ Cloudinary Connected");
 
-    // const port = process.env.PORT || 3000; // Fallback to 3000 if PORT is not set
-    // app.listen(port, "0.0.0.0", () => {
-    //   console.log(`🚀 Server running on http://localhost:${port}`);
-    // });
   } catch (error) {
     console.error("❌ Error starting the server:", error);
-    process.exit(1); // Exit process on failure
+    process.exit(1);
   }
 };
 
